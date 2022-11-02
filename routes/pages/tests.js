f2b71cba6d0e0a31b7d6976804b639dd1333ce78
bri <bsoriano1@mail.sfsu.edu>
2022-11-01 20:17:44 -0700

    user table added

--- routes/pages/tests.js
+++ routes/pages/tests.js
@@ -2,6 +2,18 @@ const express = require("express");
 const router = express.Router();
 const db = require("../../db");
 
+// router.get("/", (request, response) => {
+//   db.any(
+//     `INSERT INTO "Users" ("username", "password", "email") VALUES ('briget', '1234', 'bbso@gmail.com')`
+//   )
+//     .then((_) => db.any(`SELECT * FROM "Users"`))
+//     .then((results) => response.json(results))
+//     .catch((error) => {
+//       console.log(error);
+//       response.json({ error });
+//     });
+// });
+
 router.get("/", (request, response) => {
   db.any(
     `INSERT INTO test_table ("testString") VALUES ('Hello at ${Date.now()}')`
