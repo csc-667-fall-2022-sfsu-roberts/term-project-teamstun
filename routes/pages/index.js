319dbe56e5163d7ba0f5722e955bbc3ef48eca16
unknown <jasonnguyen717@yahoo.com>
2022-12-02 01:09:26 -0800

    updated routes and gui

--- routes/pages/index.js
+++ routes/pages/index.js
@@ -1,9 +1,40 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(request, response) {
-  response.render('public/index', { title: 'Express', layout: 'public/landing'});
+router.get("/", function (request, response) {
+  response.render("public/index", {
+    title: "TEAMSTUN",
+    layout: "public/landing",
+  });
+});
+
+router.get("/index", function (request, response) {
+  response.render("public/index", {
+    title: "TEAMSTUN",
+    layout: "public/landing",
+  });
+});
+
+router.get("/login", function (request, response) {
+  response.render("public/login", {
+    title: "Log in",
+    layout: "public/landing",
+  });
+});
+
+router.get("/lobby", function (request, response) {
+  response.render("public/lobby", {
+    title: "Lobby",
+    layout: "public/landing",
+  });
+});
+
+router.get("/signup", function (request, response) {
+  response.render("public/signup", {
+    title: "Sign up",
+    layout: "public/landing",
+  });
 });
 
 module.exports = router;
